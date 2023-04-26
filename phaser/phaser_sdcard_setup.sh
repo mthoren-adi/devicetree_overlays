a3671bf09b807ee0a36d07d1f959331049259d40
Mark Thoren <mark.thoren@analog.com>
2023-04-26 08:08:21 -0700

    Switch to pyadi-iio phaser branch add hosts and hostname, set to phaser

--- phaser/phaser_sdcard_setup.sh
+++ phaser/phaser_sdcard_setup.sh
@@ -16,10 +16,52 @@ sudo mv /etc/rc.local /etc/rc_orignal.local
 sudo cp rc.local /etc/
 
 
+# Grab hosts and hostname files with hostname set to "phaser". This
+# distinguishes the phaser setup from other ADI hardware setups using the
+# default hostname of "analog"
+wget https://github.com/mthoren-adi/rpi_setup_stuff/raw/main/phaser/hosts_phaser
+wget https://github.com/mthoren-adi/rpi_setup_stuff/raw/main/phaser/hostname_phaser
+mv hosts_phaser hosts
+mv hostname_phaser hostname
+
+sudo mv /etc/hosts /etc/hosts_orig
+sudo mv /etc/hostname /etc/hostname_orig
+sudo cp hosts /etc/
+sudo cp hostname /etc/
+
 
 # Grab handy script for updating Pluto to AD9361 mode:
 wget https://github.com/mthoren-adi/rpi_setup_stuff/raw/main/phaser/pluto_update_ad9361.sh
 chmod +x pluto_update_ad9361.sh
+
+
+cd ~
+# This is still necessary as of January, 2023.
+# Uninstall existing pyadi, reinstall from source:
+sudo pip3 uninstall -y pyadi-iio
+git clone https://github.com/analogdevicesinc/pyadi-iio.git
+cd pyadi-iio
+git checkout phaser
+sudo pip install .
+
+cd ~
+
+# As of Kuiper 2021_r2, this is still necessary for some of the phaser scripts:
+sudo pip install pyqtgraph --upgrade
+
+sudo ldconfig
+
+# Handy for ssh-ing into an attached Pluto:
+sudo apt-get install sshpass
+
+# Grab Pluto firmware while we're here:
+wget https://github.com/analogdevicesinc/plutosdr-fw/releases/download/v0.35/plutosdr-fw-v0.35.zip
+
+echo "Done setting up SD card! A reboot is probably in order, sor run 'sudo reboot'"
+echo "IF you are very sure you're only going to be operating headless or by command line and want to speed the boot / shutdown process, run:"
+echo "sudo systemctl disable x11vnc.service" 
+
+
 
 ##### as of libiio 0.24 included in Kuiper 2021_r2, this is NOT necessary:
 #git clone https://github.com/analogdevicesinc/libiio.git
@@ -33,28 +75,3 @@ chmod +x pluto_update_ad9361.sh
 #wget https://github.com/mthoren-adi/devicetree_overlays/raw/main/rpi-cn0566.dtbo
 #sudo cp rpi-cn0566.dtbo /boot/overlays
 # sudo reboot
-
-cd ~
-# This is still necessary as of January, 2023.
-# Uninstall existing pyadi, reinstall from source:
-sudo pip3 uninstall -y pyadi-iio
-git clone https://github.com/analogdevicesinc/pyadi-iio.git
-cd pyadi-iio
-git checkout cn0566_dev
-sudo python3 setup.py install
-
-cd ~
-
-sudo ldconfig
-
-sudo pip install pyqtgraph
-
-# Handy for ssh-ing into an attached Pluto:
-sudo apt-get install sshpass
-
-# Grab Pluto firmware while we're here:
-wget https://github.com/analogdevicesinc/plutosdr-fw/releases/download/v0.35/plutosdr-fw-v0.35.zip
-
-echo "Done setting up SD card! A reboot is probably in order, sor run 'sudo reboot'"
-echo "IF you are very sure you're only going to be operating headless or by command line and want to speed the boot / shutdown process, run:"
-echo "sudo systemctl disable x11vnc.service" 
